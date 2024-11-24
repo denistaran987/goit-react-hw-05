4aae7c77ca277f4c78140800ebdc5549eeefa9a1
DeniS <denistaran482@gmail.com>
2024-11-24 20:22:30 +0200

    add ButtonUp and ButtonChangeTheme

---
+++ src/components/ButtonChangeTheme/ButtonChangeTheme.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import s from './ButtonChangeTheme.module.css';
+
+const ButtonChangeTheme = () => {
+  const [isRight, setIsRight] = useState(false);
+
+  const handleClick = () => {
+    setIsRight(prevState => !prevState);
+  };
+
+  return (
+    <button
+      className={`${s['btn-header']} ${isRight ? `${s.right}` : ''}`}
+      id="btn-header"
+      onClick={handleClick}
+    >
+      <span className={s.circle} id="header-mob-span"></span>
+    </button>
+  );
+};
+export default ButtonChangeTheme;
